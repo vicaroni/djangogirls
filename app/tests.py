6cb8a088dbad56da2e9a0fac28997278b7a15635
vicaroni <vica95.a@hotmail.it>
2017-07-07 10:15:54 +0200

    Aggiunti test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,94 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .models import Post, Comment
 
 # Create your tests here.
+class PostListTest(TestCase):
+
+    def test_no_post(self):
+        user = User.objects.create(username='admin')
+        user.set_password('123456')
+        user.save()
+        self.client.login(username='admin', password='123456')
+        response = self.client.get(reverse('app:post_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['posts'], [])
+        response = self.client.get(reverse('app:post_draft_list'))
+        self.assertQuerysetEqual(response.context['posts'], [])
+
+    def test_post(self):
+        user = User.objects.create(username='admin')
+        user.set_password('123456')
+        user.save()
+        self.client.login(username='admin', password='123456')
+        Post.objects.create(author=user, title='Title', published_date=timezone.now())
+        response = self.client.get(reverse('app:post_list'))
+        self.assertQuerysetEqual(response.context['posts'], ['<Post: Title>'])
+        response = self.client.get(reverse('app:post_draft_list'))
+        self.assertQuerysetEqual(response.context['posts'], [])
+
+    def test_draft(self):
+        user = User.objects.create(username='admin')
+        user.set_password('123456')
+        user.save()
+        Post.objects.create(author=user, title='Draft', created_date=timezone.now())
+        response = self.client.get(reverse('app:post_list'))
+        self.assertQuerysetEqual(response.context['posts'], [])
+        self.client.login(username='admin', password='123456')
+        response = self.client.get(reverse('app:post_draft_list'))
+        self.assertQuerysetEqual(response.context['posts'], ['<Post: Draft>'])
+
+    def test_post_draft(self):
+        user = User.objects.create(username='admin')
+        user.set_password('123456')
+        user.save()
+        self.client.login(username='admin', password='123456')
+        Post.objects.create(author=user, title='Title', published_date=timezone.now())
+        Post.objects.create(author=user, title='Draft', created_date=timezone.now())
+        response = self.client.get(reverse('app:post_list'))
+        self.assertQuerysetEqual(response.context['posts'], ['<Post: Title>'])
+        response = self.client.get(reverse('app:post_draft_list'))
+        self.assertQuerysetEqual(response.context['posts'], ['<Post: Draft>'])
+
+    def test_2_post(self):
+        user = User.objects.create(username='admin')
+        user.set_password('123456')
+        user.save()
+        self.client.login(username='admin', password='123456')
+        Post.objects.create(author=user, title='Title1', published_date=timezone.now())
+        Post.objects.create(author=user, title='Title2', published_date=timezone.now())
+        response = self.client.get(reverse('app:post_list'))
+        self.assertQuerysetEqual(response.context['posts'], ['<Post: Title1>', '<Post: Title2>'])
+        response = self.client.get(reverse('app:post_draft_list'))
+        self.assertQuerysetEqual(response.context['posts'], [])
+
+class DetailTest(TestCase):
+
+    def test_post_detail(self):
+        user = User.objects.create(username='admin')
+        p = Post.objects.create(author=user, title='Title', text='Text', published_date=timezone.now())
+        resp = self.client.get(reverse('app:post_detail', args=(p.id,)))
+        self.assertContains(resp, p.text)
+
+    def test_comment_nonappr(self):
+        user = User.objects.create(username='admin')
+        user.set_password('123456')
+        user.save()
+        p = Post.objects.create(author=user, title='Title', text='Text', published_date=timezone.now())
+        c = Comment.objects.create(post=p, author='author', text='Comment')
+        url = reverse('app:post_detail', args=(p.id,))
+        resp = self.client.get(url)
+        self.assertContains(resp, 'No comments here yet :(')
+        self.client.login(username='admin', password='123456')
+        resp = self.client.get(url)
+        self.assertContains(resp, c.text)
+
+    def test_comment_appr(self):
+        user = User.objects.create(username='admin')
+        p = Post.objects.create(author=user, title='Title', text='Text', published_date=timezone.now())
+        c = Comment.objects.create(post=p, author='author', text='Comment')
+        c.approve()
+        resp = self.client.get(reverse('app:post_detail', args=(p.id,)))
+        self.assertContains(resp, c.text)
--- app/views.py
+++ app/views.py
@@ -21,7 +21,7 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('app:post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'app/post_edit.html', {'form': form})
@@ -36,7 +36,7 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('app:post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'app/post_edit.html', {'form': form})
@@ -50,13 +50,13 @@ def post_draft_list(request):
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
-    return redirect('post_detail', pk=pk)
+    return redirect('app:post_detail', pk=pk)
 
 @login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
-    return redirect('post_list')
+    return redirect('app:post_list')
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -66,7 +66,7 @@ def add_comment_to_post(request, pk):
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('app:post_detail', pk=post.pk)
     else:
         form = CommentForm()
     return render(request, 'app/add_comment_to_post.html', {'form': form})
@@ -75,10 +75,10 @@ def add_comment_to_post(request, pk):
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
+    return redirect('app:post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
-    return redirect('post_detail', pk=comment.post.pk)
+    return redirect('app:post_detail', pk=comment.post.pk)
